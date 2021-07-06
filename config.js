3de97307651736c09d52ef2806cf10461fe15f79
longnt98 <86997669+longnt98@users.noreply.github.com>
2021-07-06 11:08:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hey babe!',
+    introDesc: `Ngoài kia có hơn 7 tỏi người
+    Nhưng với anh
+    Nụ cười của em luôn là đẹp nhất!!`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Anh có thể biết tại sao hemm? :3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
